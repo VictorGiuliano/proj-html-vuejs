e282e5e6506ea91e67e9c47d5106547f4ef86784
VictorGiuliano <victorgiuliano98@gmail.com>
2023-01-31 18:37:27 +0100

    Complete Header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,17 @@
 <script>
 import Navbar from './utilities/Navbar.vue'
+import GenericButton from './utilities/GenericButton.vue';
 export default {
     name: 'AppMain',
     data() {
         return { informations: [{ text: 'Home' }, { text: 'Order Online' }, { text: 'About' }, { text: 'News' }, { text: 'News' }, { text: 'Contact Us' }, { object: '<font-awesome-icon icon="fa-solid fa-cart-shopping />' }] }
     },
-    components: { Navbar }
+    components: { Navbar, GenericButton },
+    methods: {
+        CreateMessage() {
+            console.log('Hai cliccato un tasto')
+        }
+    }
 }
 </script>
 <template>
@@ -13,23 +19,43 @@ export default {
         <div class="container">
             <navbar :info="informations"></navbar>
             <div class="text-area">
-                <h2>Hungry?</h2>
+                <p>Hungry?</p>
                 <h1>Great Food</h1>
-                <h1>Delivered</h1>
-
+                <h1><span>Delivered</span></h1>
             </div>
+            <div>
+                <generic-button @press-button="CreateMessage"></generic-button>
+            </div>
+
         </div>
     </section>
 </template>
 <style lang="scss" scoped>
+@use '../assets/scss/partials/variables' as *;
+
 section {
     background-image: url('../assets/img/home-background-hero-scaled.jpg');
     background-size: cover;
     background-repeat: no-repeat;
-    min-height: 600px;
-}
+    height: 600px;
 
-.text-area {
-    padding-top: 10rem;
+    .text-area {
+        padding-top: 8rem;
+    }
+
+    h1 {
+        color: $yellow;
+        font-size: 4rem;
+    }
+
+    h2,
+    p {
+        color: $white;
+        font-size: 2.5rem;
+    }
+
+    div {
+        margin-top: 2rem;
+    }
 }
 </style>
---
+++ src/components/utilities/GenericButton.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'Button',
+    emits: ['press-button'],
+    methods: {
+        pressButton() {
+            this.$emit('press-button')
+        }
+    }
+}
+</script>
+<template>
+    <form @submit.prevent=pressButton>
+        <button>WIEW OUR MENU</button>
+    </form>
+
+</template>
+<style lang="scss" scoped>
+@use '../../assets/scss/partials/variables' as *;
+</style>
