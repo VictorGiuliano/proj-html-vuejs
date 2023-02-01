80b905dc2fcb86bee5572f447a94a4fa72ee5a8a
VictorGiuliano <victorgiuliano98@gmail.com>
2023-02-01 13:09:36 +0100

    Start section Menu

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -15,38 +15,54 @@ export default {
 }
 </script>
 <template>
-    <section id="Dishes">
-        <div class="container">
-            <div class="container-cardOne">
-                <div id="#cartOne">
-                    <header>
-                        <ul>
-                            <li><font-awesome-icon icon="fa-solid fa-clock" />24/7 DELIVERY</li>
-                            <li><font-awesome-icon icon="fa-solid fa-burger" />OVER 100 DISHES</li>
-                            <li><font-awesome-icon icon="fa-solid fa-mobile-screen-button" />IN APP ORDERING</li>
-                            <li><font-awesome-icon icon="fa-solid fa-car" />FAST DELIVERY</li>
-                        </ul>
-                    </header>
-                    <div id="body-cartOne">
-                        <h3>Our Most Popular Dishes</h3>
-                    </div>
-                    <div class="container-card">
-                        <card-price :foods="foods"></card-price>
-                    </div>
-                    <div class="container-button">
-                        <GenericButton />
+    <section>
+        <div id="Dishes">
+            <div class="container">
+                <div class="container-cardOne">
+                    <div id="#cartOne">
+                        <header>
+                            <ul>
+                                <li><font-awesome-icon icon="fa-solid fa-clock" />24/7 DELIVERY</li>
+                                <li><font-awesome-icon icon="fa-solid fa-burger" />OVER 100 DISHES</li>
+                                <li><font-awesome-icon icon="fa-solid fa-mobile-screen-button" />IN APP ORDERING</li>
+                                <li><font-awesome-icon icon="fa-solid fa-car" />FAST DELIVERY</li>
+                            </ul>
+                        </header>
+                        <div id="body-cartOne">
+                            <h3>Our Most Popular Dishes</h3>
+                        </div>
+                        <div class="container-card">
+                            <card-price :foods="foods"></card-price>
+                        </div>
+                        <div class="container-button">
+                            <GenericButton />
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </section>
+    <section class="images">
+        <div class="con-wave">
+            <img class="wave" src="../assets/img/wave.svg">
+        </div>
+        <img class="bkg" src="../assets/img/first-order-background-scaled.jpg">
+    </section>
+    <section class="Menu">
+        <div class="container">
+            <div clas="title">
+                <H3>Menu Categories</H3>
+                <GenericButton />
+            </div>
+
+        </div>
+    </section>
+
 </template>
 <style lang="scss" scoped>
 @use '../assets/scss/partials/variables' as *;
 
-#Dishes {
-    min-height: 900px;
-}
+
 
 .svg-inline--fa {
     padding: 0 1rem;
@@ -91,5 +107,28 @@ export default {
         justify-content: center;
     }
 
+}
+
+.images {
+    margin-top: 45rem;
+}
+
+.con-wave {
+    position: relative;
+
+    .wave {
+        filter: invert(100%) drop-shadow(300px 0 200px white);
+        transform: rotate(-3.21rad) scale(2);
+        height: 100px;
+        position: absolute;
+    }
+}
+
+
+
+
+.bkg {
+    max-height: auto;
+    width: 100%;
 }
 </style>
